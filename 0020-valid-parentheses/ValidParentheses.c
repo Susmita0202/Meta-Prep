3392176d9338e1cbbd78c853870bcc5253a28ed7
Susmita Gorai <132455480+Susmita0202@users.noreply.github.com>
2024-12-25 09:46:29 +0530

    Create ValidParentheses.c

---
+++ 0020-valid-parentheses/ValidParentheses.c
@@ -0,0 +1,22 @@
+bool isValid(char* s) {
+    int n= strlen(s);
+    char* stack= (char*)malloc(n * sizeof(s));
+    int t=-1;
+    for(int i=0; s[i]; i++){
+        char ch= s[i];
+
+        if(ch == '(' || ch == '{' || ch == '['){
+            stack[++t] = ch;
+        }else{
+            if(t==-1) return false;
+            char open = stack[t--];
+            if((ch == ')' && open != '(') ||
+            (ch == '}' && open != '{') ||
+            (ch == ']' && open != '[')){
+                return false;
+            }
+            
+        }
+    }
+    return t == -1;
+}
