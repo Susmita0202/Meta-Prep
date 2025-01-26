43096ec839fff492d4625c00cb7e8ba2bc09a74c
Susmita Gorai <132455480+Susmita0202@users.noreply.github.com>
2025-01-26 09:03:48 +0530

    Create MaximumSubarray.java
    Time Complexity: O(n)
    Space Complexity: O(1)

---
+++ 0053-maximum-subarray/MaximumSubarray.java
@@ -0,0 +1,22 @@
+// Bruuite force O(N2)
+
+// optimal solution : Kadane'.s algorithem
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        int currSum = 0;
+        for(int i=0; i<nums.length; i++){
+             currSum  += nums[i];
+            maxSum = Math.max(currSum, maxSum);
+            if(currSum < 0){
+                currSum = 0;
+            }
+        }
+        return maxSum;
+    }
+}
+
+
+
+
+
