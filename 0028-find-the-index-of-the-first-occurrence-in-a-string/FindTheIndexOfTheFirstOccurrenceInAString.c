51fde9b25d112fd424d8c3f0fa634034922d50ac
Susmita Gorai <132455480+Susmita0202@users.noreply.github.com>
2025-08-01 20:07:33 +0530

    Create FindTheIndexOfTheFirstOccurrenceInAString.c

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/FindTheIndexOfTheFirstOccurrenceInAString.c
@@ -0,0 +1,16 @@
+int strStr(char* haystack, char* needle) {
+   int index=-1, curr=0;
+   for(int i=0; haystack[i]; i++){
+    if(needle[curr]==haystack[i]){
+        if(index==-1) index=i;
+        curr++;
+        if(!needle[curr])return index;
+    }else{
+        if(index!=-1) i=index;
+        index=-1;
+        curr=0;
+    }
+   }
+   return -1;
+
+}
